24bde9ad9d57ecf417606c0f17ce16b51a00d829
Manob Biswas <manob7@hotmail.com>
2025-01-28 17:27:18 +0600

    Activity register updated/

--- app/src/main/java/com/palmscanner/pos/RegisterActivity.java
+++ app/src/main/java/com/palmscanner/pos/RegisterActivity.java
@@ -13,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.palmscanner.pos.callback.PalmRegistrationCallback;
+import com.palmscanner.pos.fragments.PaymentAmount;
+import com.palmscanner.pos.fragments.RegisterPalm;
 import com.palmscanner.pos.threads.PalmRegistrationThread;
 import com.palmscanner.pos.viewmodel.RegisterViewModel;
 import com.palmscanner.pos.viewmodel.datatype.PalmMaskedImage;
@@ -27,7 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
     public static final String TAG = "__RegisterActivity__";
     private SDPVUnifiedAPI unifiedAPI;
     private RegisterViewModel mRegisterViewModel;
-
+    private PalmRegistrationThread registrationThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,12 +45,21 @@ public class RegisterActivity extends AppCompatActivity {
 
         //At last
         mRegisterViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
+
+        this.initSDK();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_register_container_view, RegisterPalm.class, null)
+                    .commit();
+        }
     }
 
     public void initSDK() {
         //TODO: init sdk
         unifiedAPI.initDevice(this);
-        PalmRegistrationThread registrationThread = new PalmRegistrationThread(this, new PalmRegistrationCallback() {
+         registrationThread = new PalmRegistrationThread(this, new PalmRegistrationCallback() {
             @Override
             public void onRegistrationSuccess(byte[] palmToken, List<byte[]> images) {
                 String palmTokenBase64 = null;
@@ -65,9 +76,19 @@ public class RegisterActivity extends AppCompatActivity {
 
             @Override
             public void onImageCaptured(byte[] imageBitmap) {
-                mRegisterViewModel.setMaskedImage(new PalmMaskedImage(imageBitmap, Constant.CWidth, Constant.CHeight));
+                if (mRegisterViewModel != null) {
+                    mRegisterViewModel.setMaskedImage(new PalmMaskedImage(imageBitmap, Constant.CWidth, Constant.CHeight));
+                }
             }
         }, 20);
         registrationThread.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(this.registrationThread != null){
+            this.registrationThread.stopRegistration();
+        }
     }
 }
